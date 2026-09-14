20b3052381fbbc7363babf4401098f422548f82b
chen.tao <hilerchyn@gmail.com>
2026-09-14 14:15:05 +0800

    feat: 追加chapter13试验文件

--- 13-vma-lma/main.c
+++ 13-vma-lma/main.c
@@ -1,6 +1,17 @@
 int global_value = 0x12345678;
+int zero_value;
+
+extern char _data_load[];
+extern char _sdata[];
+extern char _edata[];
 
 int main(void)
 {
-	return global_value & 0xff;
+//	memcpy(
+//			_sdata,
+//			_data_load,
+//			_edata - _sdata
+//	      );
+//
+	return global_value + zero_value;
 }
